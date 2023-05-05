4f578f6a369b2f653fb3b8e8a3f6be749215c5d4
infinissible <infinissible@gmail.com>
2023-05-05 00:28:20 -0700

    updated about page

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Layout from './Layout';
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg'>
+      <Layout className='py-8 flex items-center justify-between'>
+        <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+        <div className='flex items-center'>
+          Build with by &nbsp;
+          <Link
+            href='https://jinventor.com'
+            className='underline underline-offset-2'
+          >
+            Jinventor
+          </Link>
+        </div>
+
+        <Link
+          href='https://jinventor.com'
+          target={'_blank'}
+          className='underline underline-offset-2'
+        >
+          Say hello
+        </Link>
+      </Layout>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import profilePic from '../../public/images/profile/developer-pic-1.png';
 import AnimatedText from '@/components/AnimatedText';
 import Link from 'next/link';
 import { LinkArrow } from '@/components/Icons';
+import HireMe from '@/components/HireMe';
+import lightBulb from '../../public/images/svgs/miscellaneous_icons_1.svg';
 
 export default function Home() {
   return (
@@ -51,6 +53,11 @@ export default function Home() {
             </div>
           </div>
         </Layout>
+
+        <HireMe />
+        <div className='absolute right-8 bottom-8 inline-block w-24'>
+          <Image src={lightBulb} alt='Jinventor' className='w-full h-auto' />
+        </div>
       </main>
     </>
   );
