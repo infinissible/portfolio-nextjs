95c2ac2157508f10598e68730907d5dfb223153d
infinissible <infinissible@gmail.com>
2023-05-06 23:19:41 -0700

    updated articles page

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -5,19 +5,32 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import article1 from '../../public/images/articles/pagination component in reactjs.jpg';
+import article2 from '../../public/images/articles/create loading screen in react js.jpg';
+import { motion } from 'framer-motion';
+
+const FramerImage = motion(Image);
 
 const FeaturedArticle = ({ img, title, time, summary, link }) => {
   return (
-    <li className='col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl'>
+    <li className='relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl'>
+      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl' />
       <Link
         href={link}
         target='_blank'
-        className='w-full cursor-pointer overflow-hidden rounded-lg'
+        className='w-full inline-block cursor-pointer overflow-hidden rounded-lg'
       >
-        <Image src={img} alt={title} className='w-full h-auto' />
+        <FramerImage
+          src={img}
+          alt={title}
+          className='w-full h-auto'
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+        />
       </Link>
       <Link href={link} target='_blank'>
-        <h2 className='capitalize text-2xl font-bold my-2'>{title}</h2>
+        <h2 className='capitalize text-2xl font-bold my-2 hover:underline'>
+          {title}
+        </h2>
       </Link>
       <p className='text-sm mb-2'>{summary}</p>
       <span className='text-primary font-semibold'>{time}</span>
@@ -36,7 +49,7 @@ const articles = () => {
       <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden'>
         <Layout className='pt-16'>
           <AnimatedText text='Words Can Change The World!' className='mb-16' />
-          <ul className='grid grid-col-2 gap-16'>
+          <ul className='grid grid-cols-2 gap-16'>
             <FeaturedArticle
               title='Build A Custom Pagination Component In Reactjs From Scratch'
               summary='Learn how to build a custom pagination component in ReactJS from scratch. 
@@ -44,6 +57,15 @@ const articles = () => {
               time='9 min read'
               link='/'
               img={article1}
+            />
+
+            <FeaturedArticle
+              title='Build A Custom Pagination Component In Reactjs From Scratch'
+              summary='Learn how to build a custom pagination component in ReactJS from scratch. 
+            Follow this step-by-step guide to integrate Pagination component in your ReactJS project.'
+              time='9 min read'
+              link='/'
+              img={article2}
             />
 
             <li>Featured articles-2</li>
