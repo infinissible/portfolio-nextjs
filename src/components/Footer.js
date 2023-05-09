5b498e24188e111e2a72fed1de11f335992e98e8
infinissible <infinissible@gmail.com>
2023-05-09 00:38:01 -0700

    updated media query

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light'>
-      <Layout className='py-8 flex items-center justify-between'>
+    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light sm:text-base'>
+      <Layout className='py-8 flex items-center justify-between lg:flex-col lg:py-6'>
         <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
         <div className='flex items-center'>
           Build with by &nbsp;
--- src/pages/about.js
+++ src/pages/about.js
@@ -74,6 +74,8 @@ const about = () => {
                 src={profilePic}
                 alt='Jinventor'
                 className='w-full h-auto rounded-2xl'
+                priority
+                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
               />
             </div>
             <div className='col-span-2 flex flex-col items-end justify-between'>
